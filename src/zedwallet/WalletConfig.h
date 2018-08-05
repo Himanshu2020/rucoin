a7f4f6740cf6b2d68d9e6f528e2d33769e4a269f
Himanshu <himanshu.bramhvanshi@gmail.com>
2018-08-05 21:05:01 +0530

    hi

--- src/zedwallet/WalletConfig.h
+++ src/zedwallet/WalletConfig.h
@@ -44,10 +44,10 @@ namespace WalletConfig
 
 
     /* The length of a standard address for your coin */
-    const long unsigned int standardAddressLength = 100;
+    const long unsigned int standardAddressLength = 99;
 
     /* The length of an integrated address for your coin */
-    const long unsigned int integratedAddressLength = 237;
+    const long unsigned int integratedAddressLength = 236;
 
 
     /* The mixin value to use with transactions */
